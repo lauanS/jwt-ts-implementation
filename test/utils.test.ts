0928c2b2c0470637bb89c2414186c6d9059261fa
lauanS <lauan.s.souza@gmail.com>
2024-03-27 22:36:13 -0300

    :test_tube: Adds test for the dictionaryToJSONString function

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,4 +1,4 @@
-import { base64Encode } from '@/utils';
+import { base64Encode, dictionaryToJSONString } from '@/utils';
 
 describe('base64Encode function', () => {
   it('Should encode a string to base64', () => {
@@ -23,5 +23,28 @@ describe('base64Encode function', () => {
     const encodedString = base64Encode(input);
 
     expect(encodedString).toEqual(expectedOutput);
+  });
+});
+
+describe('dictionaryToJSONString function', () => {
+  it('Should serialize a dictionary with strings and numbers correctly', () => {
+    const dictionary = {
+      key1: 'value1',
+      key2: 42,
+      key3: false
+    };
+    const expectedOutput = '{"key1":"value1","key2":42,"key3":false}';
+    const jsonString = dictionaryToJSONString(dictionary);
+    expect(jsonString).toEqual(expectedOutput);
+  });
+
+  it('Should serialize a dictionary with null and undefined values correctly', () => {
+    const dictionary = {
+      key1: null,
+      key2: undefined,
+    };
+    const expectedOutput = '{"key1":null}';
+    const jsonString = dictionaryToJSONString(dictionary);
+    expect(jsonString).toEqual(expectedOutput);
   });
 });
