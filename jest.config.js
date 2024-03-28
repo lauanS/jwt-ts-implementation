aae91c0731db8274e8aa9cc31bd44876365a9727
lauanS <lauan.s.souza@gmail.com>
2024-03-27 21:37:25 -0300

    :wrench: Configures Jest path option

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,11 @@
+const { pathsToModuleNameMapper } = require('ts-jest')
+const { compilerOptions } = require('./tsconfig')
+
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  roots: ['<rootDir>'],
+  modulePaths: [compilerOptions.baseUrl],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
 };
